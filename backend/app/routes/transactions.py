24ad0817cd2d6eabf820fb9f4cfe8496b45ea78f
janandhinibattina <janandhinibattina@gmail.com>
2026-04-08 15:39:07 +0530

    Update transactions.py

--- backend/app/routes/transactions.py
+++ backend/app/routes/transactions.py
@@ -1,1 +1,8 @@
 # transactions routes
+from app.services.autopay_detector import test_function@router.get("/transactions")
+def get_transactions():
+    result = test_function()
+
+    return {
+        "message": result
+    }
